0b6a842ca5763e596d110e8de777b5c57deed63d
fuzzinator75 <fuzzinator75@gmail.com>
2025-07-25 22:52:39 -0400

    .

---
+++ Discord tracking bot/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Discord.Interactions;
+
+// See https://aka.ms/new-console-template for more information
+// Captain hook webook https://discord.com/api/webhooks/1091435254780268674/26Oh14QmvZhTcYYsGDg4VVAdyl491cnFlkKNxS1TvlxG1GAJe_oSM4sNoMbIfJMsRMW5
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var services = new ServiceCollection()
+            .AddSingleton<InteractionService>()
+            .AddSingleton<MessageServices>()
+            .AddSingleton<BotService>()
+            .AddSingleton<FileService>(new FileService("Suggestions.txt"))
+            .BuildServiceProvider();
+
+        var botService = services.GetRequiredService<BotService>();
+        await botService.RunAsync(services);
+    }
+}
---
+++ Discord tracking bot/SlashService.cs
@@ -0,0 +1,23 @@
+﻿
+    public class SlashService
+    {
+        private readonly FileService _fileService;  
+        public void AddSuggestion(string user, string suggestion)
+        {
+            _fileService.AddSuggestion(user, suggestion);
+        }
+
+        public string GetAllSuggestions()
+        {
+           
+            return _fileService.GetAllSuggestions();
+        }
+
+        public void DeleteSuggestion(int index)
+        {
+            _fileService.DeleteSuggestion(index);
+        }
+
+
+    }
+
